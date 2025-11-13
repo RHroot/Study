a83f33c16262af2b8985b393d3afaad6e2a7d607
r181104 <sten181104@gmail.com>
2025-11-14 05:25:49 +0530

    s

---
+++ Java/my-java-app/src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example;
+
+/** Hello world! */
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello World! This is the first program I ever wrote in nixos in Java");
+    int x = 10;
+    int y = 20;
+    System.out.println("The sum of x and y is: " + (x + y));
+  }
+}
